0ed328933891691a1d353721e9f55bdca770ddab
saadinsta <156900172+saadinsta@users.noreply.github.com>
2024-01-22 02:14:32 +0200

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 
 from langchain.agents import initialize_agent, AgentType
@@ -25,11 +24,15 @@ if "messages" not in st.session_state:
     ]
 
 for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg["content"])
+    if msg["role"] == "assistant":
+        st.text(msg["content"])
+    elif msg["role"] == "user":
+        st.text(f"User: {msg['content']}")
 
-if prompt := st.chat_input(placeholder="Who won the Women's U.S. Open in 2018?"):
+prompt = st.chat_input(placeholder="Who won the Women's U.S. Open in 2018?")
+if prompt is not None:
     st.session_state.messages.append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
+    st.text(f"User: {prompt}")
 
     if not openai_api_key:
         st.info("Please add your OpenAI API key to continue.")
@@ -38,8 +41,8 @@ if prompt := st.chat_input(placeholder="Who won the Women's U.S. Open in 2018?")
     llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=openai_api_key, streaming=True)
     search = DuckDuckGoSearchRun(name="Search")
     search_agent = initialize_agent([search], llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, handle_parsing_errors=True)
-    with st.chat_message("assistant"):
-        st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
-        response = search_agent.run(st.session_state.messages, callbacks=[st_cb])
-        st.session_state.messages.append({"role": "assistant", "content": response})
-        st.write(response)
+    
+    response = search_agent.run(st.session_state.messages, callbacks=[StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)])
+    
+    st.session_state.messages.append({"role": "assistant", "content": response})
+    st.text(f"Assistant: {response}")
